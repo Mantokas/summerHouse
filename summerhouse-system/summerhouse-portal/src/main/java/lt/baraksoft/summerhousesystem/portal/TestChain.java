045803f2cf6bec91de2fba79d97887a2c5ab7d4d
Žygimantas <zygimantas.skuolis@gmail.com>
2016-04-01 00:46:28 +0300

    added java8 specific hibernate dependancy + cleanup
    dabar pasileidžia appsas! idi rabotat bitch

--- summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summerhousesystem/portal/TestChain.java
+++ summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summerhousesystem/portal/TestChain.java
@@ -1,14 +1,7 @@
 package lt.baraksoft.summerhousesystem.portal;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.ejb.Stateless;
-import javax.inject.Named;
-
 import lt.baraksoft.summersystem.dao.impl.UserDaoImpl;
 
-@Named
-@Stateless
 public class TestChain {
 
 	private String text = "stuff";
@@ -16,16 +9,6 @@ public class TestChain {
 
 	public void doStuff() {
 		System.out.println(this.getClass().getName() + " *******");
-	}
-
-	@PostConstruct
-	public void init() {
-		System.out.println(toString() + " constructed.");
-	}
-
-	@PreDestroy
-	public void aboutToDie() {
-		System.out.println(toString() + " ready to die.");
 	}
 
 	public String getText() {
