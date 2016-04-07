0074b56843eb5147b80fcd6511a3576a5e4ab291
Martynas Žuravliovas <martynas.zuravliovas@gmail.com>
2016-04-07 23:32:30 +0300

    master: bandom daryt kad pastartuotu su dao importu

--- summerhouse-system/summerhouse-dao/src/main/java/lt/baraksoft/summersystem/dao/impl/UserDaoImpl.java
+++ summerhouse-system/summerhouse-dao/src/main/java/lt/baraksoft/summersystem/dao/impl/UserDaoImpl.java
@@ -1,13 +1,11 @@
 package lt.baraksoft.summersystem.dao.impl;
 
-import javax.ejb.Stateless;
-
 import lt.baraksoft.summersystem.dao.UserDao;
 import lt.baraksoft.summersystem.dao.generic.GenericDao;
 import lt.baraksoft.summersystem.model.User;
 
 // @SessionScoped
-@Stateless
+//@Stateless
 // Transactional (man atrodo by default)
 public class UserDaoImpl extends GenericDao<User, Integer> implements UserDao {
 
