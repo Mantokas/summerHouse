164047924a378c8614f266d7a387c3db763f5fec
Martynas Žuravliovas <martynas.zuravliovas@gmail.com>
2016-04-18 18:37:20 +0300

    master: added save() method

--- summerhouse-system/summerhouse-dao/src/main/java/lt/baraksoft/summersystem/dao/UserDao.java
+++ summerhouse-system/summerhouse-dao/src/main/java/lt/baraksoft/summersystem/dao/UserDao.java
@@ -2,7 +2,10 @@ package lt.baraksoft.summersystem.dao;
 
 import javax.ejb.Local;
 
+import lt.baraksoft.summersystem.model.User;
+
 @Local
 public interface UserDao {
 
+	void save(User entity);
 }
