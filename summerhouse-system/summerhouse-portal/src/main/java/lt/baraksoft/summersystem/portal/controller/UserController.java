d71b933c3143d984795c34a5a7369aaa2c8c424e
Martynas Žuravliovas <martynas.zuravliovas@gmail.com>
2016-04-18 19:28:37 +0300

    fix

--- summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summersystem/portal/controller/UserController.java
+++ summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summersystem/portal/controller/UserController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summersystem/portal/controller/UserController.java
 package lt.baraksoft.summersystem.portal.controller;
 
 import javax.faces.bean.ManagedBean;
@@ -9,43 +8,18 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class UserController {
 
-    private String email;
-    private String password;
+	private String email;
+	private String password;
 
-    public String getEmail() {
-        return email;
-    }
-    public String getPassword() {
-        return password;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void loginUser(String email, String password){
+	public String getPassword() {
+		return password;
+	}
 
-    }
+	public void loginUser(String email, String password) {
+
+	}
 }
-=======
-package lt.baraksoft.summersystem.portal.controller;
-
-import javax.faces.bean.ManagedBean;
-
-/**
- * Created by LaurynasC on 2016-04-18.
- */
-@ManagedBean
-public class UserController {
-
-    private String email;
-    private String password;
-
-    public String getEmail() {
-        return email;
-    }
-    public String getPassword() {
-        return password;
-    }
-
-    public void loginUser(String email, String password){
-
-    }
-}
->>>>>>> origin/master:summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summersystem/portal/controller/UserController.java
