0444fa60c56d0b0b68856b01f1d44b74bab5d862
LaurynasC <lowranho@gmail.com>
2016-04-27 00:39:44 +0300

    Lauris mldc

--- summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summersystem/portal/controller/RegistrationController.java
+++ summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summersystem/portal/controller/RegistrationController.java
@@ -2,6 +2,7 @@ package lt.baraksoft.summersystem.portal.controller;
 
 import lt.baraksoft.summersystem.portal.helper.UserViewHelper;
 import lt.baraksoft.summersystem.portal.view.UserView;
+import org.primefaces.context.RequestContext;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -10,19 +11,23 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by etere on 2016-04-25.
  */
 @ManagedBean
 @ViewScoped
-public class RegistrationController {
+public class RegistrationController implements Serializable{
+    private static final long serialVersionUID = 4283433840276008899L;
 
     @Inject
     private UserViewHelper userViewHelper;
     private UserView view;
 
     private boolean bbs;
+    private boolean dialogVisible;
 
     @PostConstruct
     public void init() {
@@ -30,14 +35,19 @@ public class RegistrationController {
     }
 
     public void registerUser(){
-
         bbs = userViewHelper.register(view);
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        List<FacesMessage> messagesList = context.getMessageList();
+        if (messagesList.isEmpty()){
+            RequestContext context2 = RequestContext.getCurrentInstance();
+            context2.execute("PF('confirmDialog').show();");
+        }
     }
 
     public void addExistingEmailMessage(){
         if(bbs)
         FacesContext.getCurrentInstance().addMessage(":userRegistrationForm:messages", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Toks el. paštas jau egzistuoja!", ""));
-
     }
 
     public UserView getUserView() {
@@ -54,5 +64,13 @@ public class RegistrationController {
 
     public void setBbs(boolean bbs) {
         this.bbs = bbs;
+    }
+
+    public boolean isDialogVisible() {
+        return dialogVisible;
+    }
+
+    public void setDialogVisible(boolean dialogVisible) {
+        this.dialogVisible = dialogVisible;
     }
 }
