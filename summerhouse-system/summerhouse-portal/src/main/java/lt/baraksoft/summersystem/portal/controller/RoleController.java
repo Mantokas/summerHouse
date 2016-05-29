1b812f0ffb35548acae1e6aa86aff3178841967e
LaurynasC <lowranho@gmail.com>
2016-05-30 00:26:39 +0300

    Rodyti mokejimus prisijungusiam

---
+++ summerhouse-system/summerhouse-portal/src/main/java/lt/baraksoft/summersystem/portal/controller/RoleController.java
@@ -0,0 +1,15 @@
+package lt.baraksoft.summersystem.portal.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * Created by LaurynasC on 2016-05-30.
+ */
+@RequestScoped
+@Named
+public class RoleController {
+
+
+
+}
